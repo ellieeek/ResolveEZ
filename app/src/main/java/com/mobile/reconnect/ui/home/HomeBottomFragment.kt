b5dd319dd642d38fb756b47d59ee21ab061621d1
dkyuuum <dkyuuum@gmail.com>
2024-10-06 23:43:32 +0900

    [ADD] 홈 바텀 시트 세팅

---
+++ app/src/main/java/com/mobile/reconnect/ui/home/HomeBottomFragment.kt
@@ -0,0 +1,19 @@
+package com.mobile.reconnect.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.Nullable
+import androidx.fragment.app.viewModels
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.mobile.reconnect.R
+import com.mobile.reconnect.databinding.FragmentHomeBinding
+import com.mobile.reconnect.databinding.FragmentHomeBottomBinding
+import com.mobile.reconnect.ui.home.viewmodel.HomeBottomViewModel
+import com.mobile.reconnect.ui.home.viewmodel.HomeViewModel
+import com.software.somding.presentation.common.BaseFragment
+
+class HomeBottomFragment: BaseFragment<FragmentHomeBottomBinding>(R.layout.fragment_home_bottom) {
+
+}
---
+++ app/src/main/java/com/mobile/reconnect/ui/home/viewmodel/HomeBottomViewModel.kt
@@ -0,0 +1,10 @@
+package com.mobile.reconnect.ui.home.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeBottomViewModel : ViewModel() {
+
+
+}
