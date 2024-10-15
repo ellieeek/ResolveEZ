34e5114ceb1502c1ceaa89e2d190d44539aa094a
ellieeekk <ellie@ellieui-MacBookAir.local>
2024-10-16 01:55:21 +0900

    제보 실종자 목록 2차 수정

--- app/src/main/java/com/mobile/reconnect/ui/map/HomeBottomFragment.kt
+++ app/src/main/java/com/mobile/reconnect/ui/map/HomeBottomFragment.kt
@@ -7,15 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.mobile.reconnect.data.model.BodyType
-import com.mobile.reconnect.data.model.FaceType
-import com.mobile.reconnect.data.model.Gender
 import com.mobile.reconnect.data.model.MissingPerson
-import com.mobile.reconnect.data.model.Nationality
-import com.mobile.reconnect.data.model.SpecialFeature
 import com.mobile.reconnect.databinding.FragmentHomeBottomBinding
 import com.mobile.reconnect.ui.map.adapter.MissingPersonsAdapter
-import java.time.LocalDateTime
 
 class HomeBottomFragment : BottomSheetDialogFragment() {
 
@@ -40,49 +34,6 @@ class HomeBottomFragment : BottomSheetDialogFragment() {
 	}
 
 	private fun setupRecyclerView() {
-		// 예시 데이터
-		persons = listOf(
-			MissingPerson(
-				id = 1,
-				name = "홍길동",
-				imageURL = "", // 여기에 이미지 URL을 넣을 수 있음
-				specialFeature = SpecialFeature.DEMENTIA,
-				nationality = Nationality.DOMESTIC,
-				gender = Gender.MALE,
-				age = 75,
-				height = 168,
-				weight = 70,
-				bodyType = BodyType.AVERAGE,
-				faceType = FaceType.OVAL,
-				tops = "청색 셔츠",
-				bottoms = "검정 바지",
-				shoes = "검정 운동화",
-				accessories = "안경",
-				hair = "짧은 머리",
-				lastSeenDateTime = LocalDateTime.now(),
-				lastSeenLocation = "서울 강남구"
-			),
-			MissingPerson(
-				id = 2,
-				name = "김철수",
-				imageURL = "", // 여기에 이미지 URL을 넣을 수 있음
-				specialFeature = SpecialFeature.NONE,
-				nationality = Nationality.DOMESTIC,
-				gender = Gender.MALE,
-				age = 80,
-				height = 175,
-				weight = 80,
-				bodyType = BodyType.OBESE,
-				faceType = FaceType.SQUARE,
-				tops = "흰색 셔츠",
-				bottoms = "회색 바지",
-				shoes = "검정 구두",
-				accessories = "모자",
-				hair = "긴 머리",
-				lastSeenDateTime = LocalDateTime.now(),
-				lastSeenLocation = "부산 해운대구"
-			)
-		)
 
 		adapter = MissingPersonsAdapter(persons) { person ->
 			Log.d("HomeBottomSheetFragment", "Clicked: ${person.name}")
