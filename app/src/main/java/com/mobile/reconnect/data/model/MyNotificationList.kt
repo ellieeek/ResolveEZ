44b63a0e9301e0fa675ccb2be519427160212531
Jung <102217402+al1kite@users.noreply.github.com>
2024-11-03 19:55:48 +0900

    [FEAT] 알림 관리 ui 1차 구현

---
+++ app/src/main/java/com/mobile/reconnect/data/model/MyNotificationList.kt
@@ -0,0 +1,8 @@
+package com.mobile.reconnect.data.model
+
+data class MyNotificationList (
+    val id: Int,
+    val status: String,
+    val message: String,
+    val additionalInfo: String
+)
---
+++ app/src/main/java/com/mobile/reconnect/ui/my/viewmodel/NotificationViewModel.kt
@@ -0,0 +1,21 @@
+package com.mobile.reconnect.ui.my.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.mobile.reconnect.data.model.MyNotificationList
+
+class AlarmViewModel : ViewModel() {
+
+    private val _alarmList = MutableLiveData<List<MyNotificationList>>()
+    val notificationList: LiveData<List<MyNotificationList>> get() = _alarmList
+
+    fun fetchAlarms() {
+        val exampleNotifications = listOf(
+            MyNotificationList(1, "실종", "실종자 홍길동씨가 나와 반경 2km 이내에 있을 수 있습니다", "남, 75세, 168cm, 70kg, 치매"),
+            MyNotificationList(2, "발견", "실종자 홍길동씨가 발견되었습니다", ""),
+            MyNotificationList(3, "실종", "실종자 홍길동씨가 나와 반경 2km 이내에 있을 수 있습니다", "남, 75세, 168cm, 70kg, 치매")
+        )
+        _alarmList.value = exampleNotifications
+    }
+}
